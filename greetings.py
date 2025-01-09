69de511210382ce1cd3dbd3844f7925e5e01767b
DMSK13 <dimmmasssik13@yandex.ru>
2025-01-10 00:49:22 +0300

    1st try

---
+++ greetings.py
@@ -0,0 +1,14 @@
+username = "Зюбрицкий Дмитрий"
+title = "Обучение"
+content = ("Сдача домашнего задания")
+status = "Активна"
+created_date = "8-01-2025"
+issue_date = "11-01-2025"
+
+
+print("Имя пользователя:", username)
+print("Заголовок заметки:", title)
+print("Описание заметки:", content)
+print("Статус заметки:",status)
+print("Дата создания заметки:", created_date)
+print("Дата истечения заметки:", issue_date)
