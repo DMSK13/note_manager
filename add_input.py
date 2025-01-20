66cb208e5797201d36ff0b1401e126ea3c2ae429
DMSK13 <dimmmasssik13@yandex.ru>
2025-01-21 00:15:55 +0300

    Add files via upload

--- add_input.py
+++ add_input.py
@@ -1,20 +1,20 @@
 username = input("Введите имя пользователя: ")
 content = input("Введите описание заметки: ")
 status = input("Введите статус заметки (например, 'Активна', 'Выполнена'): ")
-created_date = input("Введите дату создания заметки в формате 'день-месяц-год': ")
-issue_date = input("Введите дату истечения заметки в формате 'день-месяц-год': ")
+created_date = input("Введите дату создания заметки в формате 'дд-мм-гггг': ")
+issue_date = input("Введите дату истечения заметки в формате 'дд-мм-гггг': ")
 
 
 title1 = input("Введите первый заголовок заметки: ")
 title2 = input("Введите второй заголовок заметки: ")
 title3 = input("Введите третий заголовок заметки: ")
-titles = [title1, title2, title3]
+titles = [title1.capitalize(), title2.capitalize(), title3.capitalize()]
 
 
 print("\nВы ввели следующие данные:")
-print("Имя пользователя:", username)
+print("Имя пользователя:", username.title())
 print("Заголовки заметки:", titles)
-print("Описание заметки:", content)
-print("Статус заметки:", status)
-print("Дата создания заметки:", created_date)
-print("Дата истечения заметки:", issue_date)
+print("Описание заметки:", content.capitalize())
+print("Статус заметки:", status.capitalize())
+print("Дата создания:", created_date [:5])
+print("Дата истечения:", issue_date [:5])
--- add_list.py
+++ add_list.py
@@ -1,20 +1,20 @@
 username = input("Введите имя пользователя: ")
 content = input("Введите описание заметки: ")
 status = input("Введите статус заметки (например, 'Активна', 'Выполнена'): ")
-created_date = input("Введите дату создания заметки в формате 'день-месяц-год': ")
-issue_date = input("Введите дату истечения заметки в формате 'день-месяц-год': ")
+created_date = input("Введите дату создания заметки в формате 'дд-мм-гггг': ")
+issue_date = input("Введите дату истечения заметки в формате 'дд-мм-гггг': ")
 
 
 titles = []
 for i in range(3):
     title = input(f"Введите заголовок заметки {i + 1}: ")
-    titles.append(title) # Метод "интеграции"
+    titles.append(title.capitalize())
 
 
 print("\nВы ввели следующие данные:")
-print("Имя пользователя:", username)
+print("Имя пользователя:", username.capitalize())
 print("Заголовки заметки:", titles)
-print("Описание заметки:", content)
-print("Статус заметки:", status)
-print("Дата создания заметки:", created_date)
-print("Дата истечения заметки:", issue_date)
+print("Описание заметки:", content.capitalize())
+print("Статус заметки:", status.capitalize())
+print("Дата создания:", created_date [0:5])
+print("Дата истечения:", issue_date [0:5])
